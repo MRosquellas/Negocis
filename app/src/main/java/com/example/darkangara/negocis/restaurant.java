9f66e2911bd009e731250a96406f42ffc9d46002
MRosquellas <33115212+MRosquellas@users.noreply.github.com>
2017-11-03 10:56:41 +0100

    Restaurant i weather

---
+++ app/src/main/java/com/example/darkangara/negocis/restaurant.java
@@ -0,0 +1,13 @@
+package com.example.darkangara.negocis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class restaurant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant);
+    }
+}
