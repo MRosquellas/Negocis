37dc6a9a9bd0aa86dca6d412c886c76b28059047
Adkromosome <adko94@gmail.com>
2017-11-20 19:53:30 +0100

    Rejected commit: Default

--- app/src/main/java/com/example/darkangara/negocis/MainActivity.java
+++ app/src/main/java/com/example/darkangara/negocis/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.darkangara.negocis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void hotels(View v)
+    {
+        startActivity(new Intent(this, hotels.class));
+    }
+
+    public void movies(View v)
+    {
+        startActivity(new Intent(this, movies.class));
     }
 }
